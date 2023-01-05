9170ee92ec543d13e4e5c171d981c0706d66cdb1
vl199967 <vl199967@gmail.com>
2023-01-05 01:01:40 +0000

    creates a yaml file containing all the credentials needed for database connection, completed steps 1-4 under task3 under milestone2. So far the methods read db credentials from the yaml file, create a db engine, and return all the tables we have access to.

--- database_utils.py
+++ database_utils.py
@@ -1,2 +1,25 @@
+import yaml
+import sqlalchemy
+from sqlalchemy import inspect
+import psycopg2
+
+
 class DatabaseConnector():
     
+
+    def read_db_creds(self):
+        with open('db_creds.yaml') as f:
+            data = yaml.safe_load(f)
+        return data    
+
+    def init_db_engine(self):
+        creds = self.read_db_creds()
+        engine = sqlalchemy.create_engine(f"{'postgresql'}+{creds['DBAPI']}://{creds['RDS_USER']}:{creds['RDS_PASSWORD']}@{creds['RDS_HOST']}:{creds['RDS_PORT']}/{creds['RDS_DATABASE']}")
+        return engine 
+
+
+    def list_db_tables(self):
+        eng = self.init_db_engine()
+        eng.connect()
+        inspector = inspect(eng)
+        inspector.get_table_names()
